7b8ffb8284b3f0bf69ce01acdad0dedbf6a0e184
Raajmaathangi Sreevijay <70946588+Rsreev@users.noreply.github.com>
2022-12-02 18:20:56 -0800

    new code for App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header'
+import SideNav from './Components/SideNav';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import  Home from './Pages/Home';
+import  AddReviews from './Pages/AddReviews';
+import  ViewReviews  from './Pages/ViewReviews';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <BrowserRouter> 
+      <SideNav />
+      <div className="sideswitch">
+      <div style={{paddingLeft:205}} ></div>
+      <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/addreviews' element={<AddReviews />} />
+      <Route path='/viewreviews' element={<ViewReviews />} /> 
+      </Routes>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
